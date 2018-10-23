7aa530beef407f78ab75a9ec7382b058ee624b8d
Orte <kortes29@gmail.com>
2018-10-23 18:10:02 +0300

    first lessons

---
+++ app.js
@@ -0,0 +1,25 @@
+// alert("Hello world  ");
+//Log to the console
+console.log("Yo");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error("This is some error  ");
+console.clear();
+console.warn("This is warning");
+console.time("Hello");
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+console.timeEnd("Hello");
---
+++ types.js
@@ -0,0 +1,44 @@
+const name = "John Doe";
+console.log(typeof name);
+
+const age = 45;
+
+const hasKids = true;
+
+const car = null;
+
+let val;
+
+val = String(533);
+val = String(4 + 4);
+
+val = String(true);
+
+val = String(new Date());
+
+val = String([1, 2, 4, 5]);
+
+val = (5).toString;
+val = (true).toString;
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number('hell');
+val = Number([1, 2, 3, ]);
+
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+
+
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed());
+
+const val1 = '5';
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
