ee7ce87c5f2a0f2d18fd4e3dd8b682ad8f8ba986
Orte <kortes29@gmail.com>
2018-10-24 17:02:11 +0300

    Revert "training again"
    This reverts commit 1aaf40c1822e66199c3011e54d0efbb0b9ce33b5.

--- mathobj.js
+++ mathobj.js
@@ -1,18 +1,3 @@
 const num1 = 100;
 const num2 = 50;
 let val;
-// let string1 = '';
-// let User = "my name is "
-// for (let i = 0; i < 10; i++) {
-//     string1 = string1 + User;
-// }
-// console.log(string1);
-
-val = function() {
-    let some = Math.sqrt(num1);
-    let vel = Math.random() * num2;
-    return some + vel;
-}
-console.log(val());
-
-document.body.style.backgroundColor = "blue";
